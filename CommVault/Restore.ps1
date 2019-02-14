68934174314bae38dd56ca016d9aea47bce6f4ac
RavindraSolanki <developer@solanki.fr>
2019-02-14 12:38:26 +0000

    Add more reliable jobresult extraction from result of CommVault QList

--- CommVault/Restore.ps1
+++ CommVault/Restore.ps1
@@ -322,20 +322,78 @@ Function Invoke-CommVaultJobStatus
             throw "QList failed with error code $LASTEXITCODE"
         }
 
-        ForEach ($line in $($stdout -split "`r`n"))
+        ForEach ($line in $($stdout -split "`n"))
         {
             Write-Verbose $line
         }
 
-        $qlistSatusValues = [string]($stdout -Split "`r`n")[2]
+        $qlistSatusValues = Get-ValuesFromVisualTextTable -ColumnsDelimiter " -" -LineNumberForColumnsDelimiter 1 -LineNumberForValues 2 -MultiLinesText $stdout
 
-        $jobId = $qlistSatusValues.Substring(0, 11).Trim()
-        $status = $qlistSatusValues.Substring(11, 11).Trim()
-        $phase = $qlistSatusValues.Substring(22, 20).Trim()
-        $failureReason = $qlistSatusValues.Substring(42, 18).Trim()
-        $completedPercentage = $qlistSatusValues.Substring(60, 23).Trim()
+        $jobId = $qlistSatusValues[0].Trim()
+        $status = $qlistSatusValues[1].Trim()
+        $phase = $qlistSatusValues[2].Trim()
+        $failureReason = $qlistSatusValues[3].Trim()
+        $completedPercentage = $qlistSatusValues[4].Trim()
 
         return @{JobId = $JobId; Status = $status; Phase = $phase; FailureReason = $failureReason; CompletedPercentage = $completedPercentage; StatusRaw = $stdout}
+    }
+}
+
+Function Get-ValuesFromVisualTextTable
+{
+    [OutputType([string[]])]
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory)][string]$ColumnsDelimiter,
+        [Parameter(Mandatory)][string]$LineNumberForColumnsDelimiter,
+        [Parameter(Mandatory)][string]$LineNumberForValues,
+        [Parameter(Mandatory)][string]$MultiLinesText
+    )
+    Process
+    {
+        Write-Verbose "ColumnsDelimiter: $ColumnsDelimiter"
+        Write-Verbose "LineNumberForColumnsDelimiter: $LineNumberForColumnsDelimiter"
+        Write-Verbose "LineNumberForValues: $LineNumberForValues"
+        Write-Verbose "MultiLinesText:"
+        ForEach ($line in $($MultiLinesText -split "`n"))
+        {
+            Write-Verbose $line
+        }
+
+        $multiLinesTextArray = $MultiLinesText.Split("`n")
+        $lineWithColumnsDelimiter = $multiLinesTextArray[$LineNumberForColumnsDelimiter]
+        Write-Verbose "LineWithColumnsDelimiter: $lineWithColumnsDelimiter"
+        $lineWithValuesInColumns = $multiLinesTextArray[$LineNumberForValues]
+        Write-Verbose "LineWithValuesInColumns: $lineWithValuesInColumns"
+
+        $columnsStartIndexes = [int[]]@()
+        $currentColumnStartIndexInLine = 0
+        do
+        {
+            $columnsStartIndexes += $currentColumnStartIndexInLine
+            $currentColumnStartIndexInLine = $lineWithColumnsDelimiter.IndexOf($ColumnsDelimiter, $currentColumnStartIndexInLine) + ($ColumnsDelimiter.Length - 1)
+
+        } while ($currentColumnStartIndexInLine -gt 0)
+
+        $valuesInColumnOrder = [string[]]@()
+        $numberOfColumns = $columnsStartIndexes.Count
+        For ($index = 0 ; $index -lt $numberOfColumns ; $index++)
+        {
+            $nextColumnIndex = $index + 1
+            if ($nextColumnIndex -lt $numberOfColumns)
+            {
+                $length = $columnsStartIndexes[$nextColumnIndex] - $columnsStartIndexes[$index]
+            }
+            else
+            {
+                $length = $lineWithValuesInColumns.Length - $columnsStartIndexes[$index]
+            }
+            $value = $lineWithValuesInColumns.Substring($columnsStartIndexes[$index], $length)
+            $valuesInColumnOrder += $value
+        }
+
+        return $valuesInColumnOrder
     }
 }
 
