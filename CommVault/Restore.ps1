7a6eb99273de097b2123d841384a1e384c5ad540
RavindraSolanki <developer@solanki.fr>
2019-02-26 14:40:34 +0000

    Add CommVault path retrieval from environment variable

--- CommVault/Restore.ps1
+++ CommVault/Restore.ps1
@@ -120,14 +120,17 @@ Function Get-CommVaultBasePath
     {
         $commVaultLookupBasePaths = @("C:\Program Files\CommVault\Simpana\Base\", "C:\Program Files\CommVault\ContentStore\Base\", "C:\Program Files\CommVault\ContentStore2\Base\")
 
-        $commVaultBasePath = $null
-        $commVaultLookupBasePaths | ForEach-Object {
-            if (Test-Path $_) {
-                $commVaultBasePath = $_
+        $commVaultBasePath = $env:CV_Instance001
+        if ([string]::IsNullOrWhiteSpace($commVaultBasePath) -or (Test-Path -Path $commVaultBasePath -PathType Container) -eq $false)
+        {
+            $commVaultLookupBasePaths | ForEach-Object {
+                if (Test-Path -Path $_  -PathType Container) {
+                    $commVaultBasePath = $_
+                }
             }
         }
 
-        if ($null -eq $commVaultBasePath) {
+        if ([string]::IsNullOrWhiteSpace($commVaultBasePath)) {
             throw "CommVault seems to not be installed"
         }
 
